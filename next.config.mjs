16a61f4b67e280eb84079471273a3cbe6d3a32db
Adhin U <149654449+adhin-u@users.noreply.github.com>
2026-04-22 12:59:47 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  // If you use images from next/image, they will not be optimized 
+  // without a server. Disable it for static export:
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+module.exports = nextConfig;
