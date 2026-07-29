f84a0e0486fdf4a0263a199b8d97a098fd80948a
Adhin U <149654449+adhin-u@users.noreply.github.com>
2026-07-29 17:06:08 +0530

    Add files via upload

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function NotFound() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  )
+}
+
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   // If you use images from next/image, they will not be optimized 
   // without a server. Disable it for static export:
   images: {
